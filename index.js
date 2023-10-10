cf6d476e718151bd7fbb806d97fcd956ef1d9ada
santilagos <sntlgs@gmail.com>
2023-10-10 15:20:42 -0300

    aprobbed

--- index.js
+++ index.js
@@ -1,18 +1,122 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+mongoose.connect("mongodb+srv://santirova:0y7ljAsW163IBRJ4@fccbdd.fqnrbgf.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
 });
 
+const exerciseSchema = new Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
+});
 
+const User = mongoose.model('User', userSchema);
+const Exercise = mongoose.model('Exercise', exerciseSchema);
 
+app.use(cors());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// Crear un nuevo usuario
+app.post('/api/users', async (req, res) => {
+  try {
+    const { username } = req.body;
+    const user = new User({ username });
+    await user.save();
+    res.json({ username: user.username, _id: user._id });
+  } catch (error) {
+    res.status(400).json({ error: 'No se pudo crear el usuario' });
+  }
+});
+
+// Obtener una lista de todos los usuarios
+app.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(400).json({ error: 'No se pudo obtener la lista de usuarios' });
+  }
+});
+
+// Agregar un ejercicio a un usuario específico
+app.post('/api/users/:_id/exercises', async (req, res) => {
+  try {
+    const { description, duration, date } = req.body;
+    const userId = req.params._id;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+
+    const exercise = new Exercise({ userId, description, duration, date });
+
+    await exercise.save();
+
+    res.json({
+      _id: user._id,
+      username: user.username,
+      description: exercise.description,
+      duration: exercise.duration,
+      date: exercise.date.toDateString(),
+    });
+  } catch (error) {
+    res.status(400).json({ error: 'No se pudo agregar el ejercicio' });
+  }
+});
+
+// Obtener el registro completo de ejercicios de un usuario
+app.get('/api/users/:_id/logs', async (req, res) => {
+  try {
+    const userId = req.params._id;
+    const { from, to, limit } = req.query;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+
+    const query = { userId };
+
+    if (from && to) {
+      query.date = { $gte: new Date(from), $lte: new Date(to) };
+    }
+
+    let exercises = await Exercise.find(query).limit(parseInt(limit));
+
+    exercises = exercises.map((exercise) => ({
+      description: exercise.description,
+      duration: exercise.duration,
+      date: exercise.date.toDateString(),
+    }));
+
+    res.json({
+      _id: user._id,
+      username: user.username,
+      count: exercises.length,
+      log: exercises,
+    });
+  } catch (error) {
+    res.status(400).json({ error: 'No se pudo obtener el registro de ejercicios' });
+  }
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
